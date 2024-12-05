b92a35476ca086e30e7116b157c4da01df513972
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2024-12-05 09:21:02 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -27,3 +27,29 @@ public class MyClass {
         myObject.accessPrivateMembers();
     }
 }
+#autobuild job
+    // importing the File class
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("newFile.txt");
+
+    try {
+
+      // trying to create a file based on the object
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
